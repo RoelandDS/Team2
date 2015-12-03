03c1f245216fecada0e1a5637de36ddd68a4a68b
roelandds <Jarno.Michiels@student.pxl.be>
2015-12-03 13:44:15 +0100

    Server file

---
+++ htdocs/server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var app = http.createServer(function(req,res){
+    res.setHeader('Content-Type', 'application/json');
+    res.write(JSON.stringify({"name":"Koen & Jarno","members":[{"name":"Koen Vaes"},{"name":"Jarno Michiels"}]}));
+	res.end();
+});
+app.listen(3000);
+console.log('Server gestart op http://localhost:3000 ...');
