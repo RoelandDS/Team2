f3fd0ed7222d7c8b469318df0fbc8951bb017166
WeFknRise <Jarno.Michiels@student.pxl.be>
2015-12-03 13:18:38 +0100

    Eerste commit

---
+++ js/services/constants.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Jarno on 03/12/15.
+ */
+(function () {
+    'use strict';
+
+    angular.module('htf').constant('GLOBALS', {
+        lightStateUrl: 'http://10.0.1.3/api/1f25e6ee33196ee774fe20219a2950b/lights/13/state',
+        teamUrl: 'https://hackthefuture.herokuapp.com/dashboard/team'
+    });
+})();
